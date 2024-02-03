d73c0c562ed4d0db0e186a4bac1cb7b246928b4c
Soucieux <soucieux9697@gmail.com>
2024-02-03 11:27:32 -0500

    Project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: '<button class="btn btn-primary">Save</button>',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	template: '',
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'My-Own-Website';
+	courses$;
+
+	constructor(db: AngularFireDatabase) {
+		this.courses$ = db.list('/course').snapshotChanges();
+		// console.log(this.courses$);
+	}
 }
---
+++ src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+	production: false,
+	firebase: {
+		apiKey: 'AIzaSyBE1sWy0GCm-NxKU8Kdc5xUGOeEa3Z4FpA',
+		authDomain: 'my-own-website-2024.firebaseapp.com',
+		databaseURL: 'https://my-own-website-2024-default-rtdb.firebaseio.com',
+		projectId: 'my-own-website-2024',
+		storageBucket: 'my-own-website-2024.appspot.com',
+		messagingSenderId: '402312630147',
+        appId: '1:402312630147:web:49c4e645690f57aec5f0b6',
+        measurementId: "G-69SWNFCXCQ"
+	}
+};

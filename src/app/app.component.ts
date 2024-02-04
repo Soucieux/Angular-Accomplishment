0e6362958dc70fb921a7f07254e17324c8d11b3c
Soucieux <soucieux9697@gmail.com>
2024-02-03 21:44:10 -0500

    Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Observable, Subscription } from 'rxjs';
+import { animate, style, trigger, transition } from '@angular/animations';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
 	imports: [CommonModule, RouterOutlet],
-	template: '',
-	styleUrl: './app.component.css'
+	template: '<button @fade>Submit</button>',
+	styleUrl: './app.component.css',
+	animations: [
+		trigger('fade', [
+			transition('void => *', [
+				style({ backgroundColor: 'yellow', opacity: 0 }),
+				//If the second argeument is not provided, it will predefined as background: 'white', opacity: 1.
+				//So not necessary in this case.
+				animate(2000, style({ backgroundColor: 'white', opacity: 1 }))
+			])
+		])
+	]
 })
 export class AppComponent {
 	courses$;
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { environment } from '../environment/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideRouter(routes),
 		provideClientHydration(),
 		importProvidersFrom([
-			AngularFireModule.initializeApp(environment.firebase)
+            AngularFireModule.initializeApp(environment.firebase),
+            BrowserAnimationsModule
 		])
 	]
 };

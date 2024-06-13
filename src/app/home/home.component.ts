c5a5918026555feb6c608a3080feec7810e8c1b1
Soucieux <soucieux9697@gmail.com>
2024-06-13 16:41:59 -0400

    Added Drawer and Home page icon

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CdkMenuModule } from '@angular/cdk/menu';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CdkMenuModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

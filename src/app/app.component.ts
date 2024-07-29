873df7f32f4a05d3fc9085b5e036d3d35fddd3c8
Soucieux <soucieux9697@gmail.com>
2024-07-29 17:47:04 -0400

    Revert "R0 - Project Setup - Deployment"
    This reverts commit 7cccd6469d7f18b1a55e05f09e00517e257d0034.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { GoogleAuthProvider, getAuth } from '@firebase/auth';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule } from '@angular/router';
@@ -7,49 +6,28 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
-import { EntertainmentComponent } from './entertainment/entertainment.component';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { EntertainmentComponent } from "./entertainment/entertainment.component";
 
 @Component({
 	selector: 'root',
 	standalone: true,
 	imports: [
-		CommonModule,
-		RouterOutlet,
-		RouterModule,
-		MatSidenavModule,
-		MatButtonModule,
-		MatRippleModule,
-		EntertainmentComponent
-	],
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatRippleModule,
+    EntertainmentComponent
+],
 	templateUrl: 'app.component.html',
 	styleUrl: './app.component.css'
 })
 export class AppComponent {
-	// courses$;
+	courses$;
 
-	// constructor(db: AngularFireDatabase) {
-	// 	this.courses$ = db.list('/course').stateChanges();
-	// 	// this.courses$.subscribe(e=>console.log(e));
-	// }
-
-	constructor(private auth: AngularFireAuth) {}
-
-	ngOnInit() {
-		if (typeof window !== 'undefined') {
-			this.auth
-				.getRedirectResult()
-				.then((x) => console.log('Result: ' + x.user));
-		}
-	}
-
-	async login() {
-		await this.auth
-			.signInWithRedirect(new GoogleAuthProvider())
-			.catch((x) => console.log('error'));
-	}
-
-	logout() {
-		this.auth.signOut().then((x) => console.log(this.auth.user));
+	constructor(db: AngularFireDatabase) {
+		this.courses$ = db.list('/course').stateChanges();
+		// this.courses$.subscribe(e=>console.log(e));
 	}
 }

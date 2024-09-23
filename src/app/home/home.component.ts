2f8a1e67cdbb6d08bfcba8c372a553b08c4aa7c5
Soucieux <soucieux9697@gmail.com>
2024-09-22 23:19:49 -0400

    R0 - Project Setup - Firebase

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { CdkMenuModule } from '@angular/cdk/menu';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CdkMenuModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+	selector: 'home',
+	standalone: true,
+	imports: [],
+	templateUrl: './home.component.html',
+	styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}

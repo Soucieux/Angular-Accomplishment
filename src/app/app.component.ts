02e57d9c96bf52d04da1115d20e8eebbccb1b5a3
Soucieux <soucieux9697@gmail.com>
2024-09-22 23:19:49 -0400

    R0 - Project Setup - Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule } from '@angular/router';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
-
+// import { Database } from '@firebase/database';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
-import { EntertainmentComponent } from "./entertainment/entertainment.component";
+import {
+	signInWithPopup,
+	GoogleAuthProvider,
+	onAuthStateChanged,
+	Auth,
+	signOut,
+	User
+} from '@angular/fire/auth';
+
+import { EntertainmentComponent } from './entertainment/entertainment.component';
 
 @Component({
 	selector: 'root',
 	standalone: true,
 	imports: [
-    CommonModule,
-    RouterOutlet,
-    RouterModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatRippleModule,
-    EntertainmentComponent
-],
+		CommonModule,
+		RouterOutlet,
+		RouterModule,
+		MatSidenavModule,
+		MatButtonModule,
+		MatRippleModule,
+		EntertainmentComponent
+	],
 	templateUrl: 'app.component.html',
 	styleUrl: './app.component.css'
 })
 export class AppComponent {
-	courses$;
+	// courses$;
+	// constructor(db: AngularFireDatabase) {
+	// 	this.courses$ = db.list('/course').stateChanges();
+	// 	// this.courses$.subscribe(e=>console.log(e));
+	// }
+	currentUser?: User;
 
-	constructor(db: AngularFireDatabase) {
-		this.courses$ = db.list('/course').stateChanges();
-		// this.courses$.subscribe(e=>console.log(e));
+	constructor(private auth: Auth) {
+		/*
+        Note: This is only used in production environment
+        if (typeof window !== 'undefined') {
+			this.afAuth
+				.getRedirectResult()
+				.then((x) => console.log(x.additionalUserInfo?.profile));
+		}
+        */
 	}
+
+	ngOnInit() {
+		onAuthStateChanged(this.auth, (user) => console.log(user));
+	}
+
+	/* 
+    Note: This is only used in production environment
+    async login() {
+	 	await this.afAuth.signInWithRedirect(new GoogleAuthProvider());
+	 }
+     */
+
+	login() {
+		signInWithPopup(this.auth, new GoogleAuthProvider()).then((result) => {
+			console.log(result);
+		});
+	}
+
+	logout() {
+		signOut(this.auth);
+	}
+
+	// constructor(db: AngularFireDatabase) {
+	// 	this.courses$ = db.list('/course').stateChanges();
+	// 	// this.courses$.subscribe(e=>console.log(e));
 }

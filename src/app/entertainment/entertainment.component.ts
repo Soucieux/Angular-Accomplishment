fcede04abcb5daf4733c8cef08401ad40c1603b1
Soucieux <soucieux9697@gmail.com>
2025-02-22 09:24:22 -0500

    R1 - Entertainment - TV Shows Grid

--- src/app/entertainment/entertainment.component.ts
+++ src/app/entertainment/entertainment.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 import { tvShows, tvShow } from './tvShows';
 import { NgFor } from '@angular/common';
 
@@ -11,4 +11,41 @@ import { NgFor } from '@angular/common';
 })
 export class EntertainmentComponent {
 	tvShowsList: tvShow[] = tvShows;
+
+	constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+	@HostListener('window:resize')
+	onResize() {
+		this.updateGridLayout();
+	}
+
+	ngAfterViewInit() {
+		this.updateGridLayout();
+	}
+
+	updateGridLayout() {
+		const pageContainer =
+			//elRef is to get a collection, cannot modify the content directly.
+			this.elRef.nativeElement.getElementsByClassName('page-container');
+		if (pageContainer) {
+			let screenWidth = (pageContainer[0] as HTMLElement).clientWidth;
+			let itemsPerRow = Math.floor(screenWidth / 200);
+			let remainingSpacePerRow = screenWidth - itemsPerRow * 200;
+			console.log(screenWidth);
+			console.log(window.innerWidth);
+
+			// if (remainingSpacePerRow < 220) {
+			// 	itemsPerRow--;
+			// }
+
+			this.renderer.setStyle(
+				//document is to directly get HTML DOM which can be modified directly
+				document.getElementsByClassName(
+					'page-container'
+				)[0] as HTMLElement,
+				'grid-template-columns',
+				`repeat(${itemsPerRow}, minmax(200px, 1fr))`
+			);
+		}
+	}
 }

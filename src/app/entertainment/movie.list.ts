7510ac8193f95a62074ef8ed90b89aa69de23d20
Soucieux <soucieux9697@gmail.com>
2025-02-25 16:36:58 -0500

    R1 - Entertainment - Refactor move interface

--- src/app/entertainment/movie.list.ts
+++ src/app/entertainment/movie.list.ts
@@ -1,23 +1,32 @@
-export interface movie {
+interface MovieItemInterface {
 	name: string;
 	category: string;
+	rate: number;
+	cover: string;
 }
-
-export const movies: movie[] = [
-	{ name: '无证之罪', category: '悬疑' },
-	{ name: '沙海', category: '悬疑' },
-    { name: '长安十二时辰', category: '悬疑' },
-    { name: '谁是被害者 第一季', category: '悬疑' },
-    { name: '沉默的真相', category: '悬疑' },
-    { name: '终极笔记', category: '悬疑' },
-    { name: '御赐小仵作', category: '悬疑' },
-    { name: '猎罪图鉴', category: '悬疑' },
-    { name: '亲爱的小孩', category: '悬疑' },
-    { name: '异物志', category: '悬疑' },
-    { name: '风起陇西', category: '悬疑' },
-    { name: '她和她的她', category: '悬疑' },
-    { name: '三体', category: '悬疑' },
-    { name: '谁是被害者 第二季', category: '悬疑' },
-    { name: '白夜破晓', category: '悬疑' },
-    { name: '猎罪图鉴2', category: '悬疑' },
+export class MovieItem implements MovieItemInterface {
+	constructor(
+		public name: string,
+		public category: string,
+		public rate: number = -1,
+		public cover: string = ''
+	) {}
+}
+export var movies: MovieItem[] = [
+	new MovieItem('无证之罪', '悬疑'),
+	new MovieItem('沙海', '悬疑'),
+	new MovieItem('长安十二时辰', '悬疑'),
+	new MovieItem('谁是被害者 第一季', '悬疑'),
+	new MovieItem('沉默的真相', '悬疑'),
+	new MovieItem('终极笔记', '悬疑'),
+	new MovieItem('御赐小仵作', '悬疑'),
+	new MovieItem('猎罪图鉴', '悬疑'),
+	new MovieItem('亲爱的小孩', '悬疑'),
+	new MovieItem('异物志', '悬疑'),
+	new MovieItem('风起陇西', '悬疑'),
+	new MovieItem('她和她的她', '悬疑'),
+	new MovieItem('三体', '悬疑'),
+	new MovieItem('谁是被害者 第二季', '悬疑'),
+	new MovieItem('白夜破晓', '悬疑'),
+	new MovieItem('猎罪图鉴2', '悬疑')
 ];

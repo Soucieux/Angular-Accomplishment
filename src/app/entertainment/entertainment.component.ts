d55507664abc461e531a255e4cabcae0a4c969de
Soucieux <soucieux9697@gmail.com>
2025-03-03 17:57:47 -0500

    R1 - Entertainment - Upload movie rates and movie ID

--- src/app/entertainment/entertainment.component.ts
+++ src/app/entertainment/entertainment.component.ts
@@ -56,108 +56,121 @@ export class EntertainmentComponent {
 		// Step 1: Get the movie list (one-time retrieval) from firebase
 		let movieListSnapshot = await get(this.moviesRef);
 		// Step 2: Loop through the movieList to get latest movie details
-		let count = 0;
 		// In development mode, only server is doing the work.
 		if (isDevMode() && isPlatformServer(this.platformId)) {
+			let countMovies = 0;
 			for (const movieKey in movieListSnapshot.val()) {
 				// Delay 20 seconds for every 5 movies
-				count++;
-				if (count % 5 == 0) {
+				countMovies++;
+				if (countMovies % 5 == 0) {
 					LOG.warn(this.className, 'Delay 20 seconds for every 5 movies');
 					await firstValueFrom(timer(20000));
 				}
-				const movie = movieListSnapshot.val()[movieKey];
-				//Step 3: Searches for the specific movie and get the movie rate
-				LOG.info(this.className, `Get movie details for ${movie.title}`);
-				await this.searchMovie(movie.title).then((updatedMovieDetails) => {
-					// Step 10: Updates the movie rate to firebase if the movie rate is found
-					updatedMovieDetails === null
-						? LOG.warn(this.className, `Movie rate for ${movie.title} is not found`)
-						: update(dbRef(this.db, `movies/${movieKey}`), {
-								rate: updatedMovieDetails[0],
-								id: updatedMovieDetails[1]
-						  })
-								.then(() => {
-									LOG.info(
-										this.className,
-										`Movie rate for ${movie.title} is updated to ${updatedMovieDetails}`
-									);
-								})
-								.catch((error) =>
-									LOG.error(
-										this.className,
-										`Error while updating movie rate for ${movie.title}`,
-										error as Error
-									)
-								);
-				});
+				const movieItem = movieListSnapshot.val()[movieKey];
+				//Step 3: Start searching for the specific movie
+				LOG.info(this.className, `Start searching for movie details for ${movieItem.title}`);
+				let movieId = movieItem.id;
+				const movieIdAlreadyExist = movieItem.id ? true : false;
+				!movieId && LOG.info(this.className, `Movie ID not found, start searching for it.`);
+				// Step 3.1: If the movie ID is undefined in the database, then search for the movie ID first.
+				if (!movieId && !(movieId = await this.searchMovieId(movieItem.title))) {
+					//Step 3.2: If the result of searching movie ID is null, it means the server blocks the request
+					//due to too many requests, then skip the current iteration.
+					LOG.warn(this.className, `Skip movie rate search for ${movieItem.title}`);
+					continue;
+				}
+				// Step 5: Either the movie ID exists in the database or the movie ID is retrieved from Douban API.
+				try {
+					const movieRate = await this.searchMovieWithId(movieId, movieItem.title);
+					if (movieIdAlreadyExist) {
+						update(dbRef(this.db, `movies/${movieKey}`), {
+							rate: movieRate
+						}).then(() => {
+							LOG.info(this.className, `Movie rate for ${movieItem.title} has been updated`);
+						});
+					} else {
+						update(dbRef(this.db, `movies/${movieKey}`), {
+							rate: movieRate,
+							id: movieId
+						}).then(() => {
+							LOG.info(
+								this.className,
+								`Movie rate and movie ID for ${movieItem.title} have been updated`
+							);
+						});
+					}
+				} catch (error) {
+					LOG.error(
+						this.className,
+						`Error while updating movie rate for ${movieItem.title}`,
+						error as Error
+					);
+				}
 			}
 		}
 	}
 
 	/**
-	 * Search for the movie from the Douban API which returns a JSON object.
-	 * If the API responds with empty data, then it is due to too many requests.
-	 * If the API responds with data, then retrieves the image link and movie IDfrom the JSON object,
-	 * and then search for movie cover and movie rate
+	 * Get the movie webpage from Douban API with given movie ID.
 	 *
+	 * @param movieId - The ID of the movie to search for.
 	 * @param movieName - The name of the movie to search for.
-	 * @returns A Promise that resolves to an array containing the movie rate and the movie ID.
+	 * @returns A Promise that resolves to the movie rate as a string.
 	 */
-	private async searchMovie(movieName: string): Promise<[string, string] | null> {
-		try {
-			LOG.info(
-				this.className,
-				`${(this.platformId as string).toUpperCase()} is retrieving data from API`
-			);
-			// Step 4: searchMovie returns a Promise and wait for the retrieval to complete
-			const extractedData = await firstValueFrom(
-				this.doubanService.searchMovieJSON(movieName)
-			);
-			// Step 4.1: The API responds with empty data due to too many requests
-			if (extractedData == null || extractedData.length === 0) {
-				LOG.warn(this.className, 'API responded with empty data due to too many requests');
-			} else {
-				LOG.info(
-					this.className,
-					`Data received for ${movieName}. Extracting movie details.`
-				);
-				// Step 5: Extracts movie details
-				const movieDetails = extractedData[0];
-				let coverImageLink = movieDetails['img'].replace('\\', '');
-				let coverImageId = coverImageLink.substring(coverImageLink.lastIndexOf('/') + 1);
-				let movieId = movieDetails['id'];
-
-				// Step 6: Retrieves movie cover and then upload them to firebase storage
-				await this.searchMovieCover(coverImageId, movieName);
-				return [await this.searchMovieRate(movieId), movieId];
-			}
-			return null;
-		} catch (error) {
-			LOG.error(
-				this.className,
-				`Error while retrieving data for ${movieName}`,
-				error as Error
-			);
-			return null;
-		}
-	}
-
-	/**
-	 * Extracts the movie rate from the movie webpage HTML string using regex.
-	 *
-	 * @param movieId - The ID of the movie to get the rating for.
-	 * @returns A Promise that resolves to the movie rating as a string.
-	 */
-	private async searchMovieRate(movieId: any) {
+	private async searchMovieWithId(movieId: string, movieName: string): Promise<string> {
+		// Step 6: Get the movie webpage as a string
 		const movieWebpageAsString = await firstValueFrom(this.doubanService.searchMovie(movieId));
-		const regex = new RegExp(
+
+		// Step 7: Retrieves movie cover and then upload them to firebase storage
+		const regexForCoverImage = new RegExp('<img class="media" src="(.*?)" />', 'i');
+		const regexMatchForCoverImage = movieWebpageAsString.match(regexForCoverImage);
+		const coverImageId = regexMatchForCoverImage[1].substring(
+			regexMatchForCoverImage[1].lastIndexOf('/') + 1
+		);
+		await this.searchMovieCoverById(coverImageId, movieName);
+
+		// Step 8: Get the movie rate for the current movie
+		const regexForMovieId = new RegExp(
 			'<strong class="ll rating_num" property="v:average">(.*?)</strong>',
 			'i'
 		);
-		const regexMatch = movieWebpageAsString.match(regex);
-		// Step 9: Returns the movie rate
-		return regexMatch[1];
+		return movieWebpageAsString.match(regexForMovieId)[1];
+	}
+
+	/**
+	 * Search for the movie ID from the Douban API.
+	 * If the API responds with empty data, then it is due to too many requests.
+	 * If the API responds with data, then extracts the movie ID from the JSON object.
+	 *
+	 * @param movieName - The name of the movie to search for.
+	 * @returns A Promise that resolves to the movie ID.
+	 */
+	private async searchMovieId(movieName: string): Promise<string | null> {
+		// Step 4: Search for the movie ID from the Douban API
+		try {
+			LOG.info(
+				this.className,
+				`${(this.platformId as string).toUpperCase()} is retrieving movie ID from API`
+			);
+			// Step 4: searchMovie returns a Promise and wait for the retrieval to complete
+			const extractedData = await firstValueFrom(this.doubanService.searchMovieJSON(movieName));
+			// Step 4.1: If empty data is received, it means the API is not responding due to too many requests.
+			if (extractedData == null || extractedData.length === 0) {
+				LOG.warn(this.className, 'API responded with empty data due to too many requests');
+				return null;
+			} else {
+				// Step 4.2: If data is received, then extract the movie ID from the JSON object.
+				LOG.info(
+					this.className,
+					`Movie ID retrieved for ${movieName} with ID ${extractedData[0]['id']}`
+				);
+				// TODO: Here is assuming the first element is the desired movie details.
+				return extractedData[0]['id'];
+			}
+		} catch (error) {
+			LOG.error(this.className, `Error while retrieving movie ID for ${movieName}`, error as Error);
+			return null;
+		}
 	}
 
 	/**
@@ -165,20 +178,14 @@ export class EntertainmentComponent {
 	 *
 	 * @param coverImageId - The ID of the movie cover to search for.
 	 * @param movieName - The name of the movie to search for.
-	 * @returns A Promise that resolves to the movie cover as a string.
 	 */
-	private async searchMovieCover(coverImageId: string, movieName: string): Promise<void> {
-		LOG.info(
-			this.className,
-			`${(this.platformId as string).toUpperCase()} is searching movie cover`
-		);
+	private async searchMovieCoverById(coverImageId: string, movieName: string): Promise<void> {
+		LOG.info(this.className, `${(this.platformId as string).toUpperCase()} is searching movie cover`);
 		try {
-			// Step 7: searchMovieCover returns a Promise and wait for the retrieval to complete
-			const coverImage = await firstValueFrom(
-				this.doubanService.searchMovieCover(coverImageId)
-			);
+			// Step 6: searchMovieCover returns a Promise and wait for the retrieval to complete
+			const coverImage = await firstValueFrom(this.doubanService.searchMovieCover(coverImageId));
 
-			// Step 8: Uploads the movie cover to firebase storage
+			// Step 7: Uploads the movie cover to firebase storage
 			const storageRefer = storageRef(this.storage, `/movies/${coverImageId}`);
 			const uploadTask = uploadBytesResumable(storageRefer, coverImage);
 			uploadTask.on(
@@ -192,15 +199,11 @@ export class EntertainmentComponent {
 					);
 				},
 				() => {
-					LOG.info(this.className, `Image upload for ${movieName} has completed`);
+					LOG.info(this.className, `Image upload for ${movieName} has been completed`);
 				}
 			);
 		} catch (error) {
-			LOG.error(
-				this.className,
-				`Error while retrieving movie cover for ${movieName}`,
-				error as Error
-			);
+			LOG.error(this.className, `Error while retrieving movie cover for ${movieName}`, error as Error);
 		}
 	}
 
@@ -221,8 +224,7 @@ export class EntertainmentComponent {
 
 	private updateGridLayout(pageContainer: any) {
 		// Get item width from css
-		const itemsWidth =
-			getComputedStyle(pageContainer).getPropertyValue('--individual-item-width');
+		const itemsWidth = getComputedStyle(pageContainer).getPropertyValue('--individual-item-width');
 		const itemsGap = getComputedStyle(pageContainer).getPropertyValue('--individual-item-gap');
 
 		if (pageContainer) {

15d5556b8ed9dd4c9da0db9fbbb935ba997f92da
Soucieux <soucieux9697@gmail.com>
2025-03-04 15:11:41 -0500

    R1 - Entertainment - Complete upload images

---
+++ src/app/entertainment/firebase.storage.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Storage, ref as storageRef, uploadBytesResumable } from '@angular/fire/storage';
+import { LOG } from '../log';
+import { uploadBytes } from 'firebase/storage';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class FirebaseStorageService {
+	private readonly className = 'FirebaseStorageService';
+	constructor(private storage: Storage) {}
+
+	async uploadImageToFirebase(coverImageId: string, coverImage: Blob, movieName: string): Promise<void> {
+		try {
+			const storageRefer = storageRef(this.storage, `/movies/${coverImageId}`);
+			await uploadBytes(storageRefer, coverImage);
+		} catch (error) {
+			LOG.error(
+				this.className,
+				`Error while uploading image to firebase for ${movieName}`,
+				error as Error
+			);
+		}
+	}
+}

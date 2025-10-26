2f547d13c1111d79b5c2334a47d93e6b5ea4ee8a
Soucieux <soucieux9697@gmail.com>
2025-10-26 22:30:11 +0800

    R1 - Entertainment - Replace ngIf and ngfor with @if and @for

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatRadioModule } from '@angular/material/radio';
 import { LOG } from '../log';
 

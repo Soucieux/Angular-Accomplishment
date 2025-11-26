fa21f526c2907b6654f00eea147a66a0cd7e723d
Soucieux <soucieux9697@gmail.com>
2025-11-26 14:22:16 -0500

    R1 - Login - Initialize login page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MessageModule } from 'primeng/message';
+import { IftaLabelModule } from 'primeng/iftalabel';
+
+@Component({
+	selector: 'login',
+	standalone: true,
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		InputTextModule,
+		ButtonModule,
+		MessageModule,
+		IftaLabelModule
+	],
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+	exampleForm: FormGroup;
+	formSubmitted = false;
+
+	constructor(private fb: FormBuilder) {
+		this.exampleForm = this.fb.group({
+			username: ['', Validators.required],
+			password: ['', Validators.required]
+		});
+	}
+
+	isInvalid(controlName: string) {
+		const control = this.exampleForm.get(controlName);
+		return control?.invalid && (control.touched || this.formSubmitted);
+	}
+
+	onSubmit() {
+		this.formSubmitted = true;
+		if (this.exampleForm.valid) {
+			// handle login
+			console.log(this.exampleForm.value);
+		}
+	}
+}

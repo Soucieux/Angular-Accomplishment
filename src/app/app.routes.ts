3b46f87bb44127139ec0d744c3dcbc36c111f10e
Soucieux <soucieux9697@gmail.com>
2025-11-26 11:19:28 -0500

    R1 - Main - Update font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { EntertainmentComponent } from './entertainment/entertainment.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
-	{ path: 'entertainment', component: EntertainmentComponent }
+	{ path: 'entertainment', component: EntertainmentComponent },
+	{ path: 'login', component: LoginComponent }
 ];

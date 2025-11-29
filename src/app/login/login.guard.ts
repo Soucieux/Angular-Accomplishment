4a2287d93155d038f27911cd97b0e42b57b0d5d9
Soucieux <soucieux9697@gmail.com>
2025-11-29 11:16:17 -0500

    R1 - Login - Adjustment for mobile

--- src/app/login/login.guard.ts
+++ src/app/login/login.guard.ts
@@ -1,12 +1,12 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanMatchFn, Router } from '@angular/router';
 
-export const loginGuard: CanActivateFn = () => {
+export const loginGuard: CanMatchFn = () => {
 	const router = inject(Router);
 	if (localStorage.getItem('permission') === 'false') {
 		return true;
 	}
 
-	router.navigate(['/']);
-	return false;
+	// Ensures no history entry of login
+	return router.parseUrl('/');
 };

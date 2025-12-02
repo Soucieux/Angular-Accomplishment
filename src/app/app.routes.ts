24943d41d9b9af364f47533dcfdb2ff14054a202
Soucieux <soucieux9697@gmail.com>
2025-12-02 15:50:21 -0500

    R1 - PatchNotes - Initialize this new page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { EntertainmentComponent } from './entertainment/entertainment.component'
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { loginGuard } from './login/login.guard';
+import { PatchComponent } from './patch/patch.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'entertainment', component: EntertainmentComponent },
-	{ path: 'login', component: LoginComponent, canMatch: [loginGuard] }
+	{ path: 'login', component: LoginComponent, canMatch: [loginGuard] },
+	{ path: 'patch', component: PatchComponent }
 ];
---
+++ src/app/patch/patch.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patch',
+  imports: [],
+  templateUrl: './patch.component.html',
+  styleUrl: './patch.component.css',
+})
+export class PatchComponent {
+
+}

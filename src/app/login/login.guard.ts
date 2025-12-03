a66bd2438cbd2527d13fe6a9987141d95fd4c854
Soucieux <soucieux9697@gmail.com>
2025-12-03 16:41:27 -0500

    R1 - Entertainment - Initialize dialog for search button

--- src/app/login/login.guard.ts
+++ src/app/login/login.guard.ts
@@ -1,12 +1,14 @@
 import { inject } from '@angular/core';
 import { CanMatchFn, Router } from '@angular/router';
 
-export const loginGuard: CanMatchFn = () => {
-	const router = inject(Router);
-	if (localStorage != null && localStorage.getItem('permission') === 'false') {
-		return true;
-	}
+export const loginGuard =
+	(invert: boolean): CanMatchFn =>
+	() => {
+		const router = inject(Router);
+		if (localStorage != null && localStorage.getItem('permission') === 'false') {
+			return invert ? false : true;
+		}
 
-	// Ensures no history entry of login
-	return router.parseUrl('/');
-};
+		// Ensures no history entry of login
+		return invert ? true : router.parseUrl('/');
+	};

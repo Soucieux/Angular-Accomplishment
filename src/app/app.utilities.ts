628bdce655d03f44553cd34140e1b73a0a640f14
Soucieux <soucieux9697@gmail.com>
2025-12-08 20:57:53 -0500

    R1 - PatchNotes - Add dropdown list for status

--- src/app/app.utilities.ts
+++ src/app/app.utilities.ts
@@ -28,20 +28,27 @@ export class Utilities {
 	/**
 	 * Get current timestamp
 	 *
+	 * @param isTimeIncluded - Whether to include time in the formatted time.
 	 * @returns Formatted time
 	 */
-	public getCurrentFormattedTime() {
+	public getCurrentFormattedTime(isTimeIncluded: boolean) {
 		const now = new Date();
-		const formattedTime =
-			`${now.getFullYear()}.${(now.getMonth() + 1).toString().padStart(2, '0')}.${now
-				.getDate()
-				.toString()
-				.padStart(2, '0')} ` +
-			`${now.getHours().toString().padStart(2, '0')}:${now
+
+		let formattedTime = '';
+		if (isTimeIncluded) {
+			formattedTime = `${now.getHours().toString().padStart(2, '0')}:${now
 				.getMinutes()
 				.toString()
 				.padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
-		return formattedTime;
+		}
+
+		const formattedDate =
+			`${now.getFullYear()}.${(now.getMonth() + 1).toString().padStart(2, '0')}.${now
+				.getDate()
+				.toString()
+				.padStart(2, '0')} ` + formattedTime;
+
+		return formattedDate;
 	}
 
 	/**

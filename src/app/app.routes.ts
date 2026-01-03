35b0bd2cf4ce7cb5749b6bafaabaddff0204c7d1
Soucieux <soucieux9697@gmail.com>
2026-01-03 10:06:42 -0500

    R1 - Remainder - Initialize table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { loginGuard } from './login/login.guard';
 import { PatchComponent } from './patch/patch.component';
+import { RemainderComponent } from './remainder/remainder.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'entertainment', component: EntertainmentComponent },
 	{ path: 'login', component: LoginComponent, canMatch: [loginGuard] },
-	{ path: 'patch', component: PatchComponent }
+    { path: 'patch', component: PatchComponent },
+    { path: 'remainder', component: RemainderComponent }
 ];

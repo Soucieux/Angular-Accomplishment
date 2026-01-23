931f924a2e2c4686d33dfc0d41881a130e0820cd
Soucieux <soucieux9697@gmail.com>
2026-01-23 17:59:14 -0500

    R1 - Patch Notes - Minor changes

--- src/app/login/login.guard.ts
+++ src/app/login/login.guard.ts
@@ -1,11 +1,16 @@
-import { inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanMatchFn, Router } from '@angular/router';
 
 export const loginGuard: CanMatchFn = () => {
-	const router = inject(Router);
-	if (localStorage != null && localStorage.getItem('permission') === 'false') {
-		return true;
+	if (isPlatformBrowser(PLATFORM_ID)) {
+		const router = inject(Router);
+		if (localStorage != null && localStorage.getItem('permission') === 'false') {
+			return true;
+		}
+
+		// Navigates to the Home page and ensures no history entry of login
+		return router.parseUrl('/');
 	}
-	// Navigates to the Home page and ensures no history entry of login
-	return router.parseUrl('/');
+	return true;
 };

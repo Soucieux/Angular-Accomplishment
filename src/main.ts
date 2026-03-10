619dd96e1c74ea24854f7d7c6a2ded9b685db12b
Soucieux <soucieux9697@gmail.com>
2026-03-10 10:26:19 +0800

    R1 - Cloudbase - Add logs to debug

--- src/main.ts
+++ src/main.ts
@@ -9,43 +9,47 @@ import { LOG } from './app/common/app.logs';
 import { environment } from './environment/environment';
 
 (async () => {
-	await Utilities.checkCurrentCountry();
-	const country = Utilities.getCurrentCountry();
-	if (country == '') {
-		LOG.error('Main ', ' Country not set');
-	}
+    try {
+        const className = 'Utilities';
+            
+		await Utilities.checkCurrentCountry();
+		const country = Utilities.getCurrentCountry();
+        if (country == '') {
+            LOG.error(className, 'Country not set');
+		}
 
-	let providers = [...appConfig.providers];
+		let providers = [...appConfig.providers];
 
-	// Determine database service
-	const databaseProvider =
-		country === CN
-			? { provide: DatabaseService, useClass: CloudbaseService }
-			: { provide: DatabaseService, useClass: FirebaseService };
-	providers.push(databaseProvider);
+		// Determine database service
+		const databaseProvider =
+			country === CN
+				? { provide: DatabaseService, useClass: CloudbaseService }
+				: { provide: DatabaseService, useClass: FirebaseService };
+		providers.push(databaseProvider);
 
-	// Load and initialize required packages
-	if (country === CN) {
-		const { default: cloudbase } = await import('@cloudbase/js-sdk');
-		const app = cloudbase.init({
-			env: environment.cloudbase.envId,
-			region: environment.cloudbase.region
-		});
+		// Load and initialize required packages
+		if (country === CN) {
+			const { default: cloudbase } = await import('@cloudbase/js-sdk');
+			const app = cloudbase.init({
+				env: environment.cloudbase.envId,
+				region: environment.cloudbase.region
+			});
 
-		providers.push({ provide: CLOUDBASE, useValue: app });
-	} else {
-		const { provideFirebaseApp, initializeApp } = await import('@angular/fire/app');
-		const { provideAuth, getAuth } = await import('@angular/fire/auth');
-		const { provideDatabase, getDatabase } = await import('@angular/fire/database');
-		const { provideStorage, getStorage } = await import('@angular/fire/storage');
+			providers.push({ provide: CLOUDBASE, useValue: app });
+		} else {
+			const { provideFirebaseApp, initializeApp } = await import('@angular/fire/app');
+			const { provideAuth, getAuth } = await import('@angular/fire/auth');
+			const { provideDatabase, getDatabase } = await import('@angular/fire/database');
+			const { provideStorage, getStorage } = await import('@angular/fire/storage');
 
-		providers.push(
-			provideFirebaseApp(() => initializeApp(environment.firebase)),
-			provideStorage(() => getStorage()),
-			provideAuth(() => getAuth()),
-			provideDatabase(() => getDatabase())
-		);
-	}
-
-	await bootstrapApplication(AppComponent, { providers: providers });
+			providers.push(
+				provideFirebaseApp(() => initializeApp(environment.firebase)),
+				provideStorage(() => getStorage()),
+				provideAuth(() => getAuth()),
+				provideDatabase(() => getDatabase())
+			);
+		}
+        LOG.info(className, 'All startup completed')
+		await bootstrapApplication(AppComponent, { providers: providers });
+	} catch (error) {}
 })();

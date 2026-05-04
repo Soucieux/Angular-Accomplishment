f9f7127b39d1f2393e0a05420e7e5af12c59d860
Soucieux <soucieux9697@gmail.com>
2026-05-04 10:54:40 +0800

    R1 - General - Add environment template

---
+++ src/environment/environment-template.ts
@@ -0,0 +1,19 @@
+export const environment = {
+	production: false,
+	firebase: {
+		apiKey: 'YOUR-API-KEY',
+		authDomain: 'YOUR-DOMAIN',
+		databaseURL: 'YOUR-DATABASE-URL',
+		projectId: 'YOUR-PROJECT-ID',
+		storageBucket: 'YOUR-STORAGE-URL',
+		messagingSenderId: 'YOUR-MESSAGE-ID',
+		appId: 'YOUR-ADD-ID',
+		measurementId: 'YOUR-MEASUREMENT-ID'
+	},
+	cloudbase: {
+		envId: 'YOUR-ENV-ID',
+		bucket: 'YOUR-COS-BUCKET-ID',
+		region: 'ap-shanghai',
+		accessToken: 'YOUR-CLOUD-FUNCTION-TOKEN'
+	}
+};

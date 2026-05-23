0e899011e775ece8a0fdf70939ab72c9c40f3a55
Soucieux <soucieux9697@gmail.com>
2026-05-22 23:20:36 -0400

    R1 - General - Update packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,9 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
@@ -15,7 +14,6 @@ export const appConfig: ApplicationConfig = {
 	providers: [
 		provideRouter(routes),
 		provideClientHydration(),
-		importProvidersFrom(BrowserAnimationsModule),
 		provideAnimationsAsync(),
 		provideHttpClient(withFetch()),
 		providePrimeNG({
